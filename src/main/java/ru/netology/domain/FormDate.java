67704bdcf8b0d0bf3b599368d751b8dd8e1de7f9
AbdulovADA <lil.vlad7608@gmail.com>
2022-06-02 20:46:21 +0300

    field types changed

--- src/main/java/ru/netology/domain/FormDate.java
+++ src/main/java/ru/netology/domain/FormDate.java
@@ -1,7 +1,7 @@
 package ru.netology.domain;
 
 public class FormDate {
-    public String day;
-    public String month;
-    public String year;
+    public int day;
+    public int month;
+    public int year;
 }
--- src/main/java/ru/netology/domain/Main.java
+++ src/main/java/ru/netology/domain/Main.java
@@ -2,6 +2,7 @@ package ru.netology.domain;
 
 public class Main {
     public static void main(String[] args) {
+
         Post post = new Post();
         post.name = "Иван";
         post.passport = "4444№44444444";
@@ -10,8 +11,8 @@ public class Main {
         post.surname = "Иванов";
         post.subscription = true;
         post.birthday = new FormDate();
-        post.birthday.day = "13";
-        post.birthday.month = "06";
-        post.birthday.year = "1999";
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
     }
 }
